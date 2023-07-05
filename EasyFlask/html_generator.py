c98302ffa6ee53ae5006e7a3bac0239119c411a7
AltTabLife <alttablife19@gmail.com>
2023-07-05 15:52:55 -0500

    Remodular-ized yaml parser from html generator

--- EasyFlask/html_generator.py
+++ EasyFlask/html_generator.py
@@ -1,35 +1,10 @@
-#Big shoutout to Anthon for providing the logic for translating the yaml parser when I was ready to just write my own. https://stackoverflow.com/questions/76552508/how-to-force-yaml-to-overlook-duplicate-keys/76553845#76553845
 
-import ruamel.yaml
 from ._internal import keys
 
 
 class HTMLGenerator:
-    def __init__(self, file_in):
-        self.file_in = file_in
+    def __init__(self):
         self.closed_tag_array = keys.tag_lists().closed_tag_array
-
-        class MyConstructor(ruamel.yaml.RoundTripConstructor):
-            def construct_mapping(self, node, datatyp, deep=False):
-                if not isinstance(node, ruamel.yaml.nodes.MappingNode):
-                    raise ConstructorError(
-                        None, None, f'expected a mapping node, but found {node.id!s}', node.start_mark,
-                    )
-                ret_val = datatyp
-                for key_node, value_node in node.value:
-                    # keys can be list -> deep
-                    key = self.construct_object(key_node, deep=True)
-                    assert isinstance(key, str)
-                    value = self.construct_object(value_node, deep=deep)
-                    ret_val.append((key, value))
-                return ret_val
-
-            def construct_yaml_map(self, node):
-                data = []
-                yield data
-                self.construct_mapping(node, data, deep=True)
-
-        self.MyConstructor = MyConstructor
 
     def attr(self, data):
         ret_val = ''
@@ -80,14 +55,14 @@ class HTMLGenerator:
 
         return html_string
 
-    def generate_html(self):
-        self.MyConstructor.add_constructor(
-            'tag:yaml.org,2002:map', self.MyConstructor.construct_yaml_map
-        )
-        yaml = ruamel.yaml.YAML()
-        yaml.Constructor = self.MyConstructor
-        data = yaml.load(self.file_in)
-
-        html_string = self.html(data)
-        return html_string
-
+#    def generate_html(self):
+#        self.MyConstructor.add_constructor(
+#            'tag:yaml.org,2002:map', self.MyConstructor.construct_yaml_map
+#        )
+#        yaml = ruamel.yaml.YAML()
+#        yaml.Constructor = self.MyConstructor
+#        data = yaml.load(self.file_in)
+#
+#        html_string = self.html(data)
+#        return html_string
+#
