1054f60ecb1f849b8dc9ea104bd8119c37418281
AltTabLife <alttablife19@gmail.com>
2023-07-05 14:04:59 -0500

    init roadmap

---
+++ EasyFlask/road_map.py
@@ -0,0 +1,22 @@
+#This file is not necessary, but helps organize the logic going from piece to piece to build the website as a whole
+
+class RoadMap:
+
+    def __init__(self, file_in):
+        self.file_in = file_in
+        
+        #input from source build
+        
+
+
+#File in
+
+#Convert to construct
+
+#Pull app name and config
+
+#Pull pages
+
+#Pull Jinja
+
+#Build final html
