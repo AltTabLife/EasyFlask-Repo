6313b64c18c82077ed3a09e39916389a2a6902de
AltTabLife <alttablife19@gmail.com>
2023-07-06 18:52:15 -0500

    Commit for proper route handling and file writing.

--- EasyFlask/page_builder.py
+++ EasyFlask/page_builder.py
@@ -9,22 +9,28 @@ class PageBuilder:
             self.route_list = []
             self.jinja_construct = False #Denotes if a jinja construct is present for further processing
             self.html_construct = [] #Raw html constructor to be added/modified with jinja in the next extension
+            self.final_html_string = ''
 
     def build_pages(self, page_construct):
         #parse out page context, if there is none, return untouched
+        pages = []
 
-        first_page = self.Page()
         if page_construct[0][0] != 'page':
             return page_construct 
         else:
-            for a in page_construct[0][1]:
-                if a[0] == 'page_name':
-                    first_page.page_name = a[1]
-                if a[0] == 'route_list':
-                    first_page.route_list = a[1]
-                if a[0] == 'html':
-                    first_page.html_construct = [a]
-        return first_page            
+            for i in page_construct:
+                
+                page = self.Page()
+                for a in i[1]:
+                    if a[0] == 'page_name':
+                        page.page_name = a[1]
+                    if a[0] == 'route_list':
+                        page.route_list = a[1]
+                    if a[0] == 'html':
+                        page.html_construct = [a]
+                pages.append(page)
+                
+        return pages            
                 
                 
         
--- EasyFlask/road_map.py
+++ EasyFlask/road_map.py
@@ -5,6 +5,8 @@ from .page_builder import PageBuilder
 from .html_generator import HTMLGenerator
 
 
+from pathlib import Path
+
 def road_map(file_in):
     #input from source/debug build
     
@@ -17,15 +19,49 @@ def road_map(file_in):
     #Pull pages
     #Need to for loop through creating objects for each page, ideally inside the page_builder.py, returning one object with multiple subclasses.
     
-    pulled_pages = PageBuilder().build_pages(data_construct)
+    pulled_pages = PageBuilder().build_pages(data_construct) #array of pages
+
 
     #Pull Jinja
     #Check for jinja extensions/inclusions and pair classes together
-        
+
+
     #Build final html
     
-    #pull the final page classes from jinja, and create the html.
-    final_html = HTMLGenerator().generate_html(pulled_pages.html_construct)    
-    return final_html
+    for pages in pulled_pages:
+        pages.final_html_string = HTMLGenerator().generate_html(pages.html_construct)
+    
+
+    #Write to files
+    app_to_write = Path(f'output_src/app.py')
+    with open(app_to_write, 'w') as app_file:
+        app_string = f'''\
+#Imports
+from flask import Flask, render_template
+
+#Define namespace and config
+app = Flask(__name__)
+
+#Routes
+'''
+        for page in pulled_pages:
+            for routes in page.route_list:
+                app_string += f'''@app.route("{routes}")\n'''
+            app_string += f'''\
+def {page.page_name}():
+    return render_template("{page.page_name}.html")\n\n'''
+        app_string += f'''\
+
+if __name__ == "__main__":
+    app.run(debug=True)'''
+        app_file.write(app_string)
 
 
+
+    for page in pulled_pages:
+        template_to_write = Path(f'output_src/templates/{page.page_name}.html')
+        with open(template_to_write, 'w') as template:
+            template_string = f'''\
+                {page.final_html_string}
+            '''
+            template.write(template_string)
